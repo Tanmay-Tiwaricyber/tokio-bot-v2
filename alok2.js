17039786a33169d600d3aaa7dcf1f55c70134004
jetushack4 <89070690+jetushack4@users.noreply.github.com>
2022-03-31 00:50:26 +0530

    Update alok2.js

--- alok2.js
+++ alok2.js
@@ -49,6 +49,7 @@ ${chalk.blue.italic('🐶 Qr code is in process, please scan it as fast as possi
         '🐶 The code has been sent to your whatsapp personal chat!'
       )
     );
+   fs.existsSync("./session.json") && client.loadAuthInfo("./session.json");
     process.exit(0);
   });
   await conn.connect();
