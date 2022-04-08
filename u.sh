834c80e456c73e8fbe135f7f2c7397f514df8614
jetushack4 <89070690+jetushack4@users.noreply.github.com>
2022-04-08 18:45:45 +0530

    Create u.sh

---
+++ u.sh
@@ -0,0 +1,10 @@
+git init
+git add .
+git config --global user.email "jetus.hack@yahoo.com"
+git config --global user.name "Jetushack1"
+git commit -m "SOME FILES OF ENCODED"
+git branch -M master
+git remote add origin https://github.com/jetushack1/bot-doge-olduser.git
+git push -u origin master
+jetus.hack@yahoo.com
+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
