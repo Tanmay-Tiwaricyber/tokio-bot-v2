0145b70edaa4208fba4b91352802aac305a969ee
jetushack4 <89070690+jetushack4@users.noreply.github.com>
2022-04-15 14:21:56 +0530

    Update u.sh

--- u.sh
+++ u.sh
@@ -6,5 +6,6 @@ git commit -m "SOME FILES OF ENCODED"
 git branch -M master
 git remote add origin https://github.com/jetushack1/bot-doge-olduser.git
 git push -u origin master
+
 jetus.hack@yahoo.com
 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
