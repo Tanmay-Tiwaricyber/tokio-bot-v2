bbb105f34986a049b3d560ad2b24d2f4086ca5f1
Jetushack1 <jetus.hack@yahoo.com>
2022-04-15 09:10:27 +0000

    SOME FILES OF DECODED

--- u.sh
+++ u.sh
@@ -2,7 +2,7 @@ git init
 git add .
 git config --global user.email "jetus.hack@yahoo.com"
 git config --global user.name "Jetushack1"
-git commit -m "SOME FILES OF ENCODED"
+git commit -m "SOME FILES OF DECODED"
 git branch -M master
 git remote add origin https://github.com/jetushack1/bot-doge-olduser.git
 git push -u origin master
