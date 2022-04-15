be69dcd02c485f38ae4c51f074010599509ef02f
jetushack4 <89070690+jetushack4@users.noreply.github.com>
2022-04-15 15:55:18 +0530

    Create papa-pariyan.js

---
+++ plugins/papa-pariyan.js
@@ -0,0 +1,12 @@
+function handler(m) {
+  // Ini cuma contoh, jgn di uncomment -_-
+  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
+  this.sendContact(m.chat, '918602239106', 'σℓ∂υѕєя', m)
+ this.sendContact(m.chat, '+12892032273', 'amp', m)
+}
+handler.help = ['rowner/rcreator']
+handler.tags = ['rinfo']
+
+handler.command = /^(rowner|rcreator)$/i
+
+module.exports = handler
