22b5f0a885209ec9904b6ec6dd21335ab7395ec1
Jetushack1 <jetus.hack@yahoo.com>
2022-04-30 04:25:39 +0000

    first commit

--- plugins/getcode.js
+++ plugins/getcode.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 handler.help = ['getcode/jadibot']
 handler.tags = ['jadibot']
 handler.command = /^(getcode|jadibott)$/i
-handler.owner = false
+handler.owner = true
 handler.mods = false
 handler.premium = false
 handler.group = false
